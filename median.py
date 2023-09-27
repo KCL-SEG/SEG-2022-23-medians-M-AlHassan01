8e20ec2cff336e6461f6898cad774545b695f590
Miqdaad Al-Hassan <mm.al_hassan@outlook.com>
2023-09-27 13:33:57 +0100

    Replace mean() parameter with vararg; add forgotten floor division

--- median.py
+++ median.py
@@ -1,21 +1,21 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-def mean(numbers: list) -> float:
+def mean(*numbers: float) -> float:
     """Returns mean average of list of floating point numbers"""
     print(numbers)
     return sum(numbers) / len(numbers)
 
 def median_odd(numList:list) -> float:
     """Called by median() to calculate median from an odd sized list"""
-    midpointIndex = (len(numList) // 2)
+    midpointIndex = len(numList) // 2
     print("Odd", numList[midpointIndex])
     return numList[midpointIndex]
 
 def median_even(numlist:list) -> float:
     """Called by median() to calculate median value of an even sized list"""
-    midpointIndex = len(numlist)
-    print("Even:",numlist[midpointIndex:midpointIndex+2])
-    return mean(numlist[midpointIndex:midpointIndex+2])
+    midpointIndex = len(numlist) // 2
+    print("Even:",numlist[midpointIndex], numlist[midpointIndex+1])
+    return mean(numlist[midpointIndex], numlist[midpointIndex+1])
 
 def median(numList:list) -> float:
     """Returns median value from a list of floating point numbers"""
