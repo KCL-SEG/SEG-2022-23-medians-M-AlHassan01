f9358356d7bc3ff1aa9c977c3998623ebd45d8d3
Miqdaad Al-Hassan <mm.al_hassan@outlook.com>
2023-09-27 13:44:09 +0100

    Remove excess print statements

--- median.py
+++ median.py
@@ -2,19 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 def mean(*numbers: float) -> float:
     """Returns mean average of list of floating point numbers"""
-    print(numbers)
     return sum(numbers) / len(numbers)
 
 def median_odd(numList:list) -> float:
     """Called by median() to calculate median from an odd sized list"""
     midpointIndex = len(numList) // 2
-    print("Odd", numList[midpointIndex])
     return numList[midpointIndex]
 
 def median_even(numlist:list) -> float:
     """Called by median() to calculate median value of an even sized list"""
     midpointIndex = len(numlist) // 2
-    print("Even:",numlist[midpointIndex-1], numlist[midpointIndex])
     return mean(numlist[midpointIndex-1], numlist[midpointIndex])
 
 def median(numList:list) -> float:
@@ -34,9 +31,8 @@ def main() -> None:
             print("Some input could not be converted to a number!")
         else:
             flag = False
-    print(numbers)
-    print(sorted(numbers))
-    print(median(numbers))
+    print(f"Input list: {numbers}")
+    print(f"Median: {median(numbers)}")
 
 if __name__ == "__main__":
     main()
